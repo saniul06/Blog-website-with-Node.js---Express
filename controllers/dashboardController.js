db0a1a8d72c55cf72de309d172bf0a4de29866d4
Saniul Islam <saniul428@gmail.com>
2021-04-09 12:04:23 +0600

    create post controller for create-profile page

---
+++ controllers/dashboardController.js
@@ -0,0 +1,41 @@
+const flash = require("../utils/Flash");
+const User = require("../models/User");
+const Profile = require("../models/Profile");
+
+exports.dashboardGetController = async (req, res, next) => {
+    try {
+        const user = await Profile.findOne({ user: req.user._id });
+        if (user) {
+            res.render("pages/dashboard/dashboard", {
+                title: "dashboard",
+                message: flash.getMessage(req),
+            });
+        }
+        res.redirect("/dashboard/create-profile");
+    } catch (e) {
+        next(e);
+    }
+};
+
+exports.createProfileGetController = (req, res, next) => {
+    res.render("pages/dashboard/create-profile", {
+        title: "Create Your Profile",
+        message: flash.getMessage(req),
+    });
+};
+
+exports.editProfileGetController = async (req, res, next) => {
+    try {
+        const user = await Profile.findOne({ user: req.user_id });
+        if (user) {
+            res.render("pages/dashboard/edit-profile", {
+                title: "Edit Your Profile",
+                message: flash.getMessage(req),
+            });
+        }
+        res.redirect("/dashboard/create-profile");
+    } catch (e) {
+        next(e);
+    }
+};
+
